d91dc5c4a612aa2a9854bb5f1b712cf408a99bdd
Forrest <zyyu1990@gmail.com>
2020-12-07 11:42:25 +0800

    fix

--- TorusDirectSDKDemo/TorusDirectSDKDemo/ContentView.swift
+++ TorusDirectSDKDemo/TorusDirectSDKDemo/ContentView.swift
@@ -21,39 +21,39 @@ struct ContentView: View {
     var body: some View {
         NavigationView{
             List {
-                Section(header: Text("Single Logins")) {
-                    Group{
-                        Button(action: {
-                            let sub = SubVerifierDetails(loginType: .web,
-                                                         loginProvider: .wechat,
-                                                         clientId: "cewDD3i6F1vtHeV1KIbaxUZ8vJQjJZ8V",
-                                                         verifierName: "torus-auth0-wechat-lrc",
-                                                         redirectURL: "tdsdk://tdsdk/oauthCallback",
-                                                         jwtParams: ["domain":"torus-test.auth0.com"])
-                            
-                            let tdsdk = TorusSwiftDirectSDK(aggregateVerifierType: .singleLogin, aggregateVerifierName: "torus-auth0-wechat-lrc", subVerifierDetails: [sub], loglevel: .trace)
-                            tdsdk.triggerLogin(browserType: .external).done{ data in
-                                print("private key rebuild", data)
-                            }.catch{ err in
-                                print(err)
-                            }
-                        }, label: {
-                            Text("Wechat Login")
-                        })
-                        
-                    }
-                    
-                    
-                }
-                
+//                Section(header: Text("Single Logins")) {
+//                    Group{
+//                        Button(action: {
+//                            let sub = SubVerifierDetails(loginType: .web,
+//                                                         loginProvider: .wechat,
+//                                                         clientId: "cewDD3i6F1vtHeV1KIbaxUZ8vJQjJZ8V",
+//                                                         verifierName: "torus-auth0-wechat-lrc",
+//                                                         redirectURL: "tdsdk://tdsdk/oauthCallback",
+//                                                         jwtParams: ["domain":"torus-test.auth0.com"])
+//
+//                            let tdsdk = TorusSwiftDirectSDK(aggregateVerifierType: .singleLogin, aggregateVerifierName: "torus-auth0-wechat-lrc", subVerifierDetails: [sub], loglevel: .trace)
+//                            tdsdk.triggerLogin(browserType: .external).done{ data in
+//                                print("private key rebuild", data)
+//                            }.catch{ err in
+//                                print(err)
+//                            }
+//                        }, label: {
+//                            Text("Wechat Login")
+//                        })
+//
+//                    }
+//
+//
+//                }
+//
                 Section(header: Text("Single ID verifier")){
                     Button(action: {
                         let sub = SubVerifierDetails(loginType: .installed,
                                                      loginProvider: .google,
-                                                     clientId: "238941746713-vfap8uumijal4ump28p9jd3lbe6onqt4.apps.googleusercontent.com",
-                                                     verifierName: "google-ios",
-                                                     redirectURL: "com.googleusercontent.apps.238941746713-vfap8uumijal4ump28p9jd3lbe6onqt4:/oauthredirect")
-                        let tdsdk = TorusSwiftDirectSDK(aggregateVerifierType: .singleLogin, aggregateVerifierName: "multigoogle-torus", subVerifierDetails: [sub], loglevel: .trace)
+                                                     clientId: "466058994140-6fkq55j9gmh2rh8sigvgmnfomqn9n3v7.apps.googleusercontent.com",
+                                                     verifierName: "google",
+                                                     redirectURL: "com.googleusercontent.apps.466058994140-6fkq55j9gmh2rh8sigvgmnfomqn9n3v7:/oauthredirect")
+                        let tdsdk = TorusSwiftDirectSDK(aggregateVerifierType: .singleIdVerifier, aggregateVerifierName: "mathwallet-google-testnet", subVerifierDetails: [sub], loglevel: .trace)
                         tdsdk.triggerLogin(browserType: .external).done{ data in
                             print("private key rebuild", data)
                         }.catch{ err in
